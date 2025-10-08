ecde487066a747215c839a5b2a130f048d5432ea
progexam <qwerty21@newuu.uz>
2025-10-08 12:39:45 +0500

    just

---
+++ main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <sstream>
+#include <cmath>
+using namespace std;
+// void deposit(int& bal, int dep) {
+//
+//             bal+=dep;
+//             cout<<"$"<<dep<<" Deposited to your bank account"<<endl;
+// }
+// void withdraw(int& bal, int wit) {
+//     if(bal<wit) {
+//         cout <<"you have only "<<"$"<<bal<<" so you cannot witdraw";
+//     }
+//     bal-=wit;
+//     cout<<"$"<<wit<<" Withdrawn"<<endl;
+//
+// }
+//
+// int main() {
+// int balance = 500;
+//   int click;
+//     do {
+//
+//     cout<<"1. Deposit \n2. Withdraw\n 3. Check Balance\n 4. To stop the transaction"<<endl;
+//     cin>>click;
+//     switch (click) {
+//         case 1: int deposit1 ;
+//              cout<<"Enter the amount to withdraw : ";
+//             cin>>deposit1;
+//             deposit(balance, deposit1);
+//             break;
+//         case 2: int withdraw1;
+//             cout<<"Enter the amount to withdraw : ";
+//             cin>>withdraw1;
+//             withdraw(balance, withdraw1);
+//             if(balance<0) cout<<"Transaction aborted dute to infuccient amount check your card"<<endl;
+//             break;
+//         case 3: cout<<"Your balance is: $"<<balance<<endl; break;
+//         case 4: cout<<"Thanks for transaction!!!"; break;
+//         default: cout<<"Invalid input"<<endl;
+//     }
+//
+//     }
+//
+//     while ( click!=4);
+//   }
+
+
+//Problem2
+//  void TotalPrice(int movieprice, int total) {
+//      int fullprice = movieprice * total;
+//      cout <<"Total Price = "<<"$" << fullprice << endl;
+//  }
+// int main() {
+//           int click;
+//             do {
+//                 cout << "1. Enter to movies tickets slide \n2. Stop the Booking"<<endl;
+//                 cin >> click;
+//             switch (click) {
+//                 case 1:{ int options;
+//                         cout<<"1. Avangers: The FINALS: \n1. Titanic Redemption: \n3. Pamidori qomagan kelin: \n 4. Stop"<<endl;
+//                     cin>>options;
+//                     switch (options) {
+//                         case 1: {
+//                             int priceA=12;
+//                             cout<<"The price: "<<"$"<<priceA<<endl;
+//                             int sumA;
+//                             cout<<"How many tickets:";
+//                             cin>>sumA;
+//                             TotalPrice(priceA,sumA);
+//                             break;
+//                         }
+//                         case 2: {
+//                             int priceB=12;
+//                             cout<<"The price: "<<"$"<<priceB<<endl;
+//                             int sumB;
+//                             cout<<"How many tickets:";
+//                             cin>>sumB;
+//                             TotalPrice(priceB,sumB);
+//                             break;
+//                         }
+//                         case 3: {
+//                             int priceC=12;
+//                             cout<<"The price: "<<"$"<<priceC<<endl;
+//                             int sumC;
+//                             cout<<"How many tickets:";
+//                             cin>>sumC;
+//                             TotalPrice(priceC,sumC);
+//                             break;
+//                         }
+//                         case 4: {
+//                             cout<<"Thanks for booking";
+//                             break;
+//                         }
+//                     }
+//                     break;
+//
+//                     }
+//                 case 2: {
+//                     cout<<"huh can book at least one pls!!";
+//                     break;
+//                 }
+//             }
+//
+//             }
+//
+//             while ( click!=2);
+//           }
+
+int main() {
+
+}
